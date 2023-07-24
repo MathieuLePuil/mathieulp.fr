4ee9ec60e699e73529ce858fe2661294fdbe3de7
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-07-24 20:24:54 +0200

    :wrench: | Menu de navigation (bug dans le hover)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js,php}", "./../*.{html,js,php}"],
+    content: ["./*.{html,js,php,vue}", "./../*.{html,js,php,vue}", "./../../*.{html,js,php,vue}"],
     theme: {
         extend: {},
     },
