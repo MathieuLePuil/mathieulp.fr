49ba546cd6a45163d275f83142501b1c6a889e19
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-08-04 22:02:37 +0200

    :heavy_plus_sign: | Création du component Project.vue et importation dans HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
     import Home from '../components/Home.vue'
+    import Project from '../components/Project.vue'
 </script>
 
 <template>
     <main>
         <Home />
+        <Project />
     </main>
 </template>
