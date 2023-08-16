ed1c0f7b33f99cf842404582523c50d2937f1870
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-08-16 12:39:25 +0200

    :wrench: | Ajout des droits dans le contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,12 +3,12 @@
 </script>
 
 <template>
-    <section id="contact" class="h-m-screen lg:!h-screen pt-[8vh] lg:!pt-0 pb-20 flex flex-col justify-around z-20  lg:!w-full">
+    <section id="contact" class="h-m-screen lg:!h-screen pt-[8vh] lg:!pt-0 pb-20 flex flex-col justify-around z-20 lg:!w-full">
         <div class="w-full flex justify-start">
             <h4 class="mt-10 text-2xl sm:!text-3xl lg:!text-6xl ml-[10%] text-my-purple font-medium z-20">Contact</h4>
         </div>
         <div class="z-20 h-full">
-            <form action="#" method="POST" class="mx-auto mt-12 max-w-xl sm:mt-20">
+            <form action="#" method="POST" class="mx-auto mt-10 max-w-xl sm:mt-20">
                 <div class="w-full">
                     <div class="w-full flex flex-col justify-center">
                         <label for="prenom" class="block text-sm font-semibold leading-6 text-gray-900 px-4 lg:!px-0 ml-[8%] lg:ml-0">Prénom / Nom</label>
@@ -39,6 +39,9 @@
                     <button type="submit" class="block max-w-[80%] lg:!max-w-full w-full rounded-md !bg-my-purple px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:!bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Envoyer</button>
                 </div>
             </form>
+            <div class="w-full flex justify-center mt-16">
+                <p class="text-xs sm:text-sm">&copy; 2023 - Tous droits réservés - Mathieu Le Puil</p>
+            </div>
         </div>
     </section>
 </template>
