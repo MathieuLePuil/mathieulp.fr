b51a221031ee4fcdbfbafd15207c15f3b81380da
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-08-21 13:45:15 +0200

    :globe_with_meridians: | Ajout des pdf de présentation et des liens vers ceux-ci

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -93,7 +93,7 @@
                         <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">...</span>
                     </li>
                 </ul>
-                <button type="button" class="text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">En savoir plus</button>
+                <a href="files/Site_vitrine_avance.pdf" target="_blank" class="text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center mt-3">En savoir plus</a>
             </div>
             <!--            FIN TARIF 2             -->
 
@@ -137,7 +137,7 @@
                         <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">...</span>
                     </li>
                 </ul>
-                <button type="button" class="text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">En savoir plus</button>
+                <a href="files/Site_e-commerce.pdf" target="_blank" class="text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center mt-3">En savoir plus</a>
             </div>
             <!--            FIN TARIF 3             -->
 
