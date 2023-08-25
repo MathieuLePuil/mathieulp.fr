2ea4f5c897334e0bae370a6ee94096c8862e2704
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-08-25 21:52:33 +0200

    :wrench: | [fix] Alignment of pricing "En savoir plus" buttons

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -139,7 +139,7 @@
                         </li>
                     </ul>
                 </div>
-                <a href="files/Site_e-commerce.pdf" target="_blank" class="self-center text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-fullÈ text-center mt-3">En savoir plus</a>
+                <a href="files/Site_e-commerce.pdf" target="_blank" class="self-center text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center mt-3">En savoir plus</a>
             </div>
             <!--            FIN TARIF 3             -->
 
