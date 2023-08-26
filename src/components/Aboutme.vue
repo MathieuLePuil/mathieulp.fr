7b874298abca13bf9a97b0953c5c18b0ba7f223b
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-08-26 17:53:42 +0200

    :building_construction: | [feature] Adding translations and replacing texts in Aboutme.vue

--- src/components/Aboutme.vue
+++ src/components/Aboutme.vue
@@ -1,16 +1,16 @@
 <template>
     <section id="aboutme" class="h-m-screen sm:!h-screen pt-[8vh] lg:!pt-0 pb-20 flex flex-col justify-around z-20">
         <div class="w-full flex justify-end">
-            <h4 class="mt-10 text-2xl sm:!text-3xl lg:!text-6xl mr-[10%] text-my-purple font-medium z-20">Qui suis-je ?</h4>
+            <h4 class="mt-10 text-2xl sm:!text-3xl lg:!text-6xl mr-[10%] text-my-purple font-medium z-20">{{ $t('whoami') }}</h4>
         </div>
         <div class="flex w-full flex-col justify-center items-center lg:!flex-row z-20">
             <div class="w-[80%] flex justify-center lg:!justify-start my-10 lg:!my-0 lg:!w-[30%] lg:!ml-[10%]">
                 <img src="../assets/img/me.png" alt="Moi" class="w-full max-w-[50%] sm:!max-w-[60%]">
             </div>
             <div class="text-justify text-[16px] text-my-black w-[80%] lg:!w-[50%]  lg:!mr-[10%]">
-                <p>Je suis <span class="text-my-purple font-medium">Mathieu Le Puil</span>, j’ai <span class="text-my-purple font-medium">20 ans</span> et je suis actuellement en B.U.T Métiers du Multimédia et de l’Internet à l’I.U.T de Troyes. En parallèle, je suis <span class="text-my-purple font-medium">alternant</span> en tant que <span class="text-my-purple font-medium">développeur web Full-Stack</span> dans les auto-écoles Popeye.</p>
-                <p class="mt-4">J’ai commencé à m’intéresser à la programmation durant le premier confinement de 2020.  Je développais à ce moment-là des bots pour des serveurs Discord. Je me suis ensuite intéressé aux bases de données pour stocker les informations des utilisateurs. Passionné par le développement, j’ai décidé d’en faire mon métier.</p>
-                <p class="mt-4">Les études arrivaient à grands pas. Je me suis penché sur la filière MMI car elle regroupait tout ce que j’aimais : Le graphisme, la communication, la vidéo et surtout le développement. J’ai pu donc, au cours de mon cursus, créer plusieurs sites internet et découvrir de nombreux outils. Naturellement, j’ai pris la décision de me lancer en auto-entreprise afin d’avoir toujours des projets à faire en dehors de celui de mon travail actuel.</p>
+                <p>{{ $t('iam') }} <span class="text-my-purple font-medium">Mathieu Le Puil</span>, {{ $t('ageone') }} <span class="text-my-purple font-medium">20 {{ $t('agetwo') }}</span> {{ $t('txtformation') }} <span class="text-my-purple font-medium">{{ $t('working') }}</span> {{ $t('asa') }} <span class="text-my-purple font-medium">{{ $t('developper') }}</span> {{ $t('autoecole') }}</p>
+                <p class="mt-4">{{ $t('txtprogrammation') }}</p>
+                <p class="mt-4">{{ $t('txtstudy') }}</p>
             </div>
         </div>
     </section>
--- src/main.js
+++ src/main.js
@@ -10,15 +10,28 @@ const i18n = createI18n({
         en: {
             home: 'Home',
             projects: 'My projects',
-            whoami: "Who Am I ?",
+            whoami: "Who am I ?",
             skills: "My skills",
             prices: "My prices",
+
             homeh3: "Hi, I'm",
             homeh2: "Freelance Full-Stack Developer",
+
             portfolio: "Portfolio built to develop my image when looking for a work-study position as a Full-Stack web developer.",
             troyesevent: "Website developed during my school studies. The aim was to create a ticketing service for (fictitious) events in our town.",
             popeye: "Popeye is my work-study company. I'm in charge of redesigning the website and creating tools (admin, for example) around it.",
             websitelink: "Website Link",
+
+            iam: "I am",
+            ageone: "I am",
+            agetwo: "years old",
+            txtformation: "and I am currently in a B.U.T in Multimedia and Internet Professions at the I.U.T of Troyes. In parallel, I am",
+            working: "working",
+            asa: "as a",
+            developper: "full-stack web developer",
+            autoecole: "in the Popeye driving schools.",
+            txtprogrammation: "I first became interested in programming during the first lockdown in 2020. At that time I was developing bots for Discord servers. I then became interested in databases for storing user information. Passionate about development, I decided to make it my profession.",
+            txtstudy: "Studies were just around the corner. I decided to study the MMI programme because it brought together everything I loved: graphic design, communication, video and, above all, development. So, during my course, I was able to create several websites and learn about a wide range of tools. Naturally, I decided to set up my own business so that I'd always have projects to do outside my current job.",
         },
         fr: {
             home: 'Accueil',
@@ -26,12 +39,25 @@ const i18n = createI18n({
             whoami: "Qui suis-je ?",
             skills: "Mes compétences",
             prices: "Mes tarifs",
+
             homeh3: "Salut ! Moi c'est",
             homeh2: "Développeur Full-Stack Freelance",
+
             portfolio: "Portfolio construit pour développer mon image lors de ma recherche d'alternance en tant que développeur web.",
             troyesevent: "Site développé lors de mon cursus scolaire. L'objectif était de réalisé une billetterie pour les évènements (fictifs) de notre ville.",
             popeye: "Popeye est l'entreprise de mon alternance. Je m'occupe de la refonte du site web ainsi que la création d'outils autour de celui-ci.",
             websitelink: "Lien du site",
+
+            iam: "Je suis",
+            ageone: "j'ai",
+            agetwo: "ans",
+            txtformation: "et je suis actuellement en B.U.T Métiers du Multimédia et de l’Internet à l’I.U.T de Troyes. En parallèle, je suis",
+            working: "alternant",
+            asa: "en tant que",
+            developper: "développeur web Full-Stack",
+            autoecole: "dans les auto-écoles Popeye.",
+            txtprogrammation: "J’ai commencé à m’intéresser à la programmation durant le premier confinement de 2020. Je développais à ce moment-là des bots pour des serveurs Discord. Je me suis ensuite intéressé aux bases de données pour stocker les informations des utilisateurs. Passionné par le développement, j’ai décidé d’en faire mon métier.",
+            txtstudy: "Les études arrivaient à grands pas. Je me suis penché sur la filière MMI car elle regroupait tout ce que j’aimais : Le graphisme, la communication, la vidéo et surtout le développement. J’ai pu donc, au cours de mon cursus, créer plusieurs sites internet et découvrir de nombreux outils. Naturellement, j’ai pris la décision de me lancer en auto-entreprise afin d’avoir toujours des projets à faire en dehors de celui de mon travail actuel.",
         },
     },
 });
