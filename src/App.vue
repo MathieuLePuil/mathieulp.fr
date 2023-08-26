59a84cd4e6dc2d5451eb5e991fffed52768bd5f1
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-08-26 17:09:04 +0200

    :building_construction: | [feature] Installation of the i18n library and configuration using the navigation menu

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <script setup>
         import { RouterView, RouterLink } from 'vue-router'
+        import { ref, defineProps } from 'vue';
+        import { useI18n } from 'vue-i18n';
 </script>
 
 <template>
-  <header>
-      <nav class="border-gray-200 z-20">
-          <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 z-20">
-              <router-link to="/" class="flex items-center z-20">
+  <header class="z-30">
+      <nav class="border-gray-200 z-30">
+          <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 z-30">
+              <router-link to="/" class="flex items-center z-30">
                   <img src="./assets/img/LOGO.png" class="h-8 mr-3" alt="Logo" />
                   <span class="self-center text-2xl font-semibold whitespace-nowrap">Mathieu</span>
               </router-link>
-              <div class="flex items-center lg:order-2 z-20">
-                  <button type="button" data-dropdown-toggle="language-dropdown-menu" class="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-gray-900 rounded-lg cursor-pointer hover:bg-gray-200 z-20">
-                          <img src="./assets/img/pays/fr.png" aria-hidden="true" class="h-3.5 w-3.5 rounded-full mr-2" alt="Test">
-                          Français
-                  </button>
-                  <!-- Dropdown -->
-                  <div class="z-50 hidden my-4 text-base list-none divide-y divide-gray-100 rounded-lg shadow " id="language-dropdown-menu">
-                      <ul class="py-2 font-medium" role="none">
-                          <li>
-                              <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem">
-                                  <div class="inline-flex items-center">
+              <div class="flex items-center lg:order-2 z-30">
+                  <div class="relative">
+                      <button @click="toggleDropdown" type="button" data-dropdown-toggle="language-dropdown-menu" class="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-gray-900 rounded-lg cursor-pointer hover:bg-gray-200 z-30">
+                          <img :src="getLocaleFlagSrc()" aria-hidden="true" class="h-3.5 w-3.5 rounded-full mr-2" alt="Flag">
+                          {{ getLocaleLabel() }}
+                      </button>
+                      <!-- Dropdown -->
+                      <div v-if="isDropdownOpen" class="z-50 mt-2 text-base list-none divide-gray-100 rounded-lg shadow bg-[#F6F6F6] absolute left-0 w-full">
+                          <ul class="py-2 font-medium flex flex-col justify-around" role="none">
+                              <li>
+                                  <button @click="changeLocale('fr'); closeDropdown()" type="button" class="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-gray-900 rounded-lg cursor-pointer hover:bg-gray-200 z-30">
                                       <img src="./assets/img/pays/fr.png" aria-hidden="true" class="h-3.5 w-3.5 rounded-full mr-2" alt="Test">
                                       Français
-                                  </div>
-                              </a>
-                          </li>
-                          <li>
-                              <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200" role="menuitem">
-                                  <div class="inline-flex items-center">
-                                      <svg aria-hidden="true" class="h-3.5 w-3.5 rounded-full mr-2" xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-us" viewBox="0 0 512 512"><g fill-rule="evenodd"><g stroke-width="1pt"><path fill="#bd3d44" d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z" transform="scale(3.9385)"/><path fill="#fff" d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z" transform="scale(3.9385)"/></g><path fill="#192f5d" d="M0 0h98.8v70H0z" transform="scale(3.9385)"/><path fill="#fff" d="M8.2 3l1 2.8H12L9.7 7.5l.9 2.7-2.4-1.7L6 10.2l.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7L74 8.5l-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 7.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 24.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 21.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 38.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 35.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 52.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 49.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 66.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 63.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9z" transform="scale(3.9385)"/></g></svg>
-                                      Coming Soon...
-                                  </div>
-                              </a>
-                          </li>
-                      </ul>
+                                  </button>
+                              </li>
+                              <li>
+                                  <button @click="changeLocale('en'); closeDropdown()" type="button" class="inline-flex w-full items-center font-medium justify-center px-4 p-2 text-sm text-gray-900 rounded-lg cursor-pointer hover:bg-gray-200 z-30">
+                                      <img src="./assets/img/pays/en.png" aria-hidden="true" class="h-3.5 w-3.5 rounded-full mr-2" alt="Test">
+                                      English
+                                  </button>
+                              </li>
+                          </ul>
+                      </div>
                   </div>
                   <button data-collapse-toggle="navbar-language" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-language" aria-expanded="false">
                       <span class="sr-only">Menu principal</span>
@@ -46,19 +46,19 @@
               <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1 z-20" id="navbar-language">
                   <ul class="flex flex-col font-medium p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 z-20">
                       <li>
-                          <a href="#home" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">Accueil</a>
+                          <a href="#home" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">{{ $t('home') }}</a>
                       </li>
                       <li>
-                          <a href="#projects" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">Mes réalisations</a>
+                          <a href="#projects" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">{{ $t('projects') }}</a>
                       </li>
                       <li>
-                          <a href="#aboutme" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">Qui suis-je ?</a>
+                          <a href="#aboutme" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">{{ $t('whoami') }}</a>
                       </li>
                       <li>
-                          <a href="#skills" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">Mes compétences</a>
+                          <a href="#skills" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">{{ $t('skills') }}</a>
                       </li>
                       <li>
-                          <a href="#price" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">Mes tarifs</a>
+                          <a href="#price" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">{{ $t('prices') }}</a>
                       </li>
                       <li>
                           <a href="#contact" class="block py-2 pl-3 pr-4 text-black rounded hover:text-white hover:bg-indigo-500 z-20" aria-current="page">Contact</a>
@@ -71,3 +71,37 @@
 
   <RouterView />
 </template>
+
+<script>
+export default {
+    name: 'App',
+    setup() {
+        // Utilisation de la composition API pour accéder à l'instance de Vue-i18n
+        const { t, locale } = useI18n();
+
+        // ... le reste de votre code
+    },
+    data() {
+        return {
+            isDropdownOpen: false, // Variable pour gérer l'état du menu déroulant
+        };
+    },
+    methods: {
+        changeLocale(locale) {
+            this.$i18n.locale = locale;
+        },
+        getLocaleFlagSrc() {
+            return this.$i18n.locale === 'fr' ? '../src/assets/img/pays/fr.png' : '../src/assets/img/pays/en.png';
+        },
+        getLocaleLabel() {
+            return this.$i18n.locale === 'fr' ? 'Français' : 'English';
+        },
+        toggleDropdown() {
+            this.isDropdownOpen = !this.isDropdownOpen; // Inverse l'état du menu déroulant
+        },
+        closeDropdown() {
+            this.isDropdownOpen = false; // Ferme le menu déroulant
+        },
+    },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,34 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createI18n } from 'vue-i18n'; // Importez Vue-i18n ici
+import router from './router';
 
-const app = createApp(App)
+// Créez une instance de Vue-i18n
+const i18n = createI18n({
+    locale: 'fr', // Langue par défaut
+    messages: {
+        en: {
+            home: 'Home',
+            projects: 'My projects',
+            whoami: "Who Am I ?",
+            skills: "My skills",
+            prices: "My prices",
+        },
+        fr: {
+            home: 'Accueil',
+            projects: "Mes réalisations",
+            whoami: "Qui suis-je ?",
+            skills: "Mes compétences",
+            prices: "Mes tarifs",
+        },
+    },
+});
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+// Utilisez Vue-i18n
+app.use(i18n);
+
+app.use(router);
+
+app.mount('#app');
