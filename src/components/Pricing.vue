254ef37f2d96c8da39a65e096c02bff63c1493d9
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-08-26 18:24:18 +0200

    :building_construction: | [feature] Adding translations and replacing texts in Pricing.vue

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -1,16 +1,16 @@
 <template>
     <section id="price" class="h-m-screen lg:!h-screen pt-[8vh] lg:!pt-0 pb-20 flex flex-col justify-around z-20">
         <div class="w-full flex justify-end">
-            <h4 class="mt-10 text-2xl sm:!text-3xl lg:!text-6xl mr-[10%] text-my-purple font-medium z-20">Tarifs</h4>
+            <h4 class="mt-10 text-2xl sm:!text-3xl lg:!text-6xl mr-[10%] text-my-purple font-medium z-20">{{ $t('prices') }}</h4>
         </div>
         <div class="flex w-full flex-col justify-between lg:px-[10%] items-center lg:!flex-row z-20 mt-10">
 
             <!--            TARIF 1             -->
             <div class="w-[80%] lg:!w-full h-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 mb-8 lg:mb-0 flex flex-col justify-between">
                 <div>
-                    <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Site vitrine standard</h5>
+                    <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">{{ $t('standard') }}</h5>
                     <div class="flex items-baseline text-gray-900 dark:text-white">
-                        <span class="ml-1 text-xs font-normal text-gray-500 dark:text-gray-400 mr-3">À partir de</span>
+                        <span class="ml-1 text-xs font-normal text-gray-500 dark:text-gray-400 mr-3">{{ $t('from') }}</span>
                         <span class="text-5xl font-bold tracking-tight">499</span>
                         <span class="text-3xl font-semibold">€</span>
                     </div>
@@ -25,19 +25,19 @@
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Charte graphique fourni par le client (logo, couleurs, ...)</span>
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{{ $t('chart1') }}</span>
                         </li>
                         <li class="flex space-x-3 items-center">
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Interface administrateur standard</span>
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{{ $t('admin1') }}</span>
                         </li>
                         <li class="flex space-x-3 items-center">
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Site responsive (adapté à toute taille d'écran)</span>
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{{ $t('responsive') }}</span>
                         </li>
                         <li class="flex space-x-3 items-center">
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -47,16 +47,16 @@
                         </li>
                     </ul>
                 </div>
-                <a href="files/Site_vitrine_standard.pdf" target="_blank" class="self-center text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center mt-3">En savoir plus</a>
+                <a href="files/Site_vitrine_standard.pdf" target="_blank" class="self-center text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center mt-3">{{ $t('readmore') }}</a>
             </div>
             <!--            FIN TARIF 1             -->
 
             <!--            TARIF 2             -->
             <div class="w-[80%] lg:!w-full h-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 mb-8 lg:mx-10 lg:mb-0 flex flex-col justify-between">
                 <div>
-                    <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Site vitrine avancé</h5>
+                    <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">{{ $t('advanced') }}</h5>
                     <div class="flex items-baseline text-gray-900 dark:text-white">
-                        <span class="ml-1 text-xs font-normal text-gray-500 dark:text-gray-400 mr-3">À partir de</span>
+                        <span class="ml-1 text-xs font-normal text-gray-500 dark:text-gray-400 mr-3">{{ $t('from') }}</span>
                         <span class="text-5xl font-bold tracking-tight">799</span>
                         <span class="text-3xl font-semibold">€</span>
                     </div>
@@ -65,25 +65,25 @@
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Développé sur mesure</span>
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{{ $t('measure') }}</span>
                         </li>
                         <li class="flex space-x-3 items-center">
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Création d'une charte graphique personnalisée</span>
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{{ $t('chart2') }}</span>
                         </li>
                         <li class="flex space-x-3 items-center">
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Nom de domaine (.fr) offert pendant 1 an</span>
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{{ $t('domain') }}</span>
                         </li>
                         <li class="flex space-x-3 items-center">
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Interface administrateur sur mesure</span>
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{{ $t('admin2') }}</span>
                         </li>
                         <li class="flex space-x-3 items-center">
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -93,16 +93,16 @@
                         </li>
                     </ul>
                 </div>
-                <a href="files/Site_vitrine_avance.pdf" target="_blank" class="self-center text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center mt-3">En savoir plus</a>
+                <a href="files/Site_vitrine_avance.pdf" target="_blank" class="self-center text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center mt-3">{{ $t('readmore') }}</a>
             </div>
             <!--            FIN TARIF 2             -->
 
             <!--            TARIF 3             -->
             <div class="w-[80%] lg:!w-full h-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 mb-8 lg:mb-0 flex flex-col justify-between">
                 <div>
-                    <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Site e-commerce</h5>
+                    <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">{{ $t('ecommerce') }}</h5>
                     <div class="flex items-baseline text-gray-900 dark:text-white">
-                        <span class="ml-1 text-xs font-normal text-gray-500 dark:text-gray-400 mr-3">À partir de</span>
+                        <span class="ml-1 text-xs font-normal text-gray-500 dark:text-gray-400 mr-3">{{ $t('from') }}</span>
                         <span class="text-5xl font-bold tracking-tight">1499</span>
                         <span class="text-3xl font-semibold">€</span>
                     </div>
@@ -117,19 +117,19 @@
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Création d'une charte graphique personnalisée</span>
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{{ $t('chart2') }}</span>
                         </li>
                         <li class="flex space-x-3 items-center">
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Nom de domaine (.fr) offert pendant 1 an</span>
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{{ $t('domain') }}</span>
                         </li>
                         <li class="flex space-x-3 items-center">
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                             </svg>
-                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Interface administrateur sur mesure</span>
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{{ $t('admin2') }}</span>
                         </li>
                         <li class="flex space-x-3 items-center">
                             <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -139,7 +139,7 @@
                         </li>
                     </ul>
                 </div>
-                <a href="files/Site_e-commerce.pdf" target="_blank" class="self-center text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center mt-3">En savoir plus</a>
+                <a href="files/Site_e-commerce.pdf" target="_blank" class="self-center text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center mt-3">{{ $t('readmore') }}</a>
             </div>
             <!--            FIN TARIF 3             -->
 
@@ -151,24 +151,24 @@
                     <div class="flex items-baseline text-gray-900 dark:text-white mt-8">
                         <span class="text-5xl font-bold tracking-tight">40</span>
                         <span class="text-3xl font-semibold">€</span>
-                        <span class="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">/heure</span>
+                        <span class="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">/{{ $t('hours') }}</span>
                     </div>
                     <div class="space-y-5 mt-12">
-                        <a href="#contact" class="text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Me contacter</a>
+                        <a href="#contact" class="text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">{{ $t('contactme') }}</a>
                     </div>
                 </div>
                 <!--            FIN TARIF 4             -->
 
                 <!--            TARIF 5             -->
                 <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-5 dark:bg-gray-800 dark:border-gray-700 h-full lg:h-[33vh] xl:!h-full">
-                    <h5 class="mb-3 text-sm font-medium text-gray-500 dark:text-gray-400">Développement spécifique</h5>
+                    <h5 class="mb-3 text-sm font-medium text-gray-500 dark:text-gray-400">{{ $t('specific') }}</h5>
                     <div class="flex items-baseline text-gray-900 dark:text-white mt-6">
                         <span class="text-5xl font-bold tracking-tight">300</span>
                         <span class="text-3xl font-semibold">€</span>
-                        <span class="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">/jour</span>
+                        <span class="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">/{{ $t('day') }}</span>
                     </div>
                     <div class="space-y-5 mt-9">
-                        <a href="#contact" class="text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Me contacter</a>
+                        <a href="#contact" class="text-white !bg-my-purple hover:!bg-indigo-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">{{ $t('contactme') }}</a>
                     </div>
                 </div>
                 <!--            FIN TARIF 5             -->
