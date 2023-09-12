f735754afc80db68c591c31e70ff70870069a093
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-09-12 18:55:49 +0200

    :sparkles: | [feature] Add a require to the email and a load when the email is sent

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -8,16 +8,27 @@ const message = ref('');
 
 const contactForm = ref(null);
 
+const isSubmitting = ref(false);
+
 const submitForm = () => {
+    // Marquez le début de la soumission
+    isSubmitting.value = true;
+
     const serviceID = 'service_tafcc6f';
     const templateID = 'template_obzu7bz';
 
     if (contactForm.value) {
         emailjs.sendForm(serviceID, templateID, contactForm.value)
             .then(() => {
+                // Marquez la fin de la soumission
+                isSubmitting.value = false;
+
                 alert('Le mail a bien été envoyé !');
             })
             .catch(err => {
+                // Marquez la fin de la soumission
+                isSubmitting.value = false;
+
                 alert(JSON.stringify(err));
             });
     }
@@ -41,7 +52,7 @@ const submitForm = () => {
                     <div class="mt-2 w-full flex flex-col justify-center">
                         <label for="email" class="block text-sm font-semibold leading-6 text-gray-900 px-4 lg:!px-0 ml-[8%] lg:ml-0 mt-1">Email</label>
                         <div class="mt-1 w-full flex justify-center">
-                            <input v-model="email" type="email" name="email" id="email" autocomplete="email" class="block w-[80%] lg:!w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mx-4 lg:!mx-0">
+                            <input v-model="email" type="email" name="email" id="email" autocomplete="email" class="block w-[80%] lg:!w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mx-4 lg:!mx-0" required>
                         </div>
                     </div>
                     <div class="mt-2 w-full flex flex-col justify-center">
@@ -58,7 +69,9 @@ const submitForm = () => {
                     </div>
                 </div>
                 <div class="mt-10 flex justify-center">
-                    <button type="submit" class="block max-w-[80%] lg:!max-w-full w-full rounded-md !bg-my-purple px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:!bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 hover:scale-105 transition-all duration-200 ease">Envoyer</button>
+                    <button type="submit" class="block max-w-[80%] lg:!max-w-full w-full rounded-md !bg-my-purple px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:!bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 hover:scale-105 transition-all duration-200 ease">
+                        {{ isSubmitting ? 'Chargement...' : 'Envoyer' }}
+                    </button>
                 </div>
             </form>
             <div class="w-full flex justify-center mt-16">
