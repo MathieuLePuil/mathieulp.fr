25e609ee1e2c3c3b74275a7fe263049122ca3b64
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-09-13 21:53:59 +0200

    :wrench: | [feature] Drafting and formatting of the legal notices page

--- src/components/Mentions.vue
+++ src/components/Mentions.vue
@@ -1,143 +1,20 @@
 <template>
-    <section id="home" class="h-full pt-[8vh] lg:!pt-0 lg:!h-[92vh]">
-        <div class="h-full w-full flex flex-col justify-center pl-[10%] pb-[5%] z-20">
-            <h3 class="z-20 text-xl sm:!text-3xl lg:!text-4xl font-medium text-my-black mb-1 lg:mb-3">{{ $t('homeh3') }}</h3>
-            <h1 class="z-20 text-4xl sm:!text-7xl lg:!text-9xl font-bold text-my-purple mb-1 lg:mb-3">Mathieu Le Puil</h1>
-            <h2 class="z-20 text-sm sm:!text-3xl lg:!text-4xl font-medium text-my-purple">{{ $t('homeh2') }}</h2>
-        </div>
-        <div class="fixed bottom-0 -left-2 sm:!left-1 px-4 flex flex-col items-center z-30">
-            <a href="mailto:contact@mathieulp.fr" target="_blank"><img src="../assets/img/email.png" class="w-4 sm:!w-8 mb-2 hover:scale-105 transition-all duration-200 ease" alt="Logo Mail" /></a>
-            <a href="https://www.linkedin.com/in/mathieulepuil/" target="_blank"><img src="../assets/img/logo_linkedin.png" class="w-4 sm:!w-8 mb-2 hover:scale-105 transition-all duration-200 ease" alt="Logo Linkedin" /></a>
-            <a href="https://www.github.com/MathieuLePuil" target="_blank"><img src="../assets/img/logo_github.png" class="w-4 sm:!w-8 mb-2 hover:scale-105 transition-all duration-200 ease" alt="Logo Github" /></a>
-            <a href="https://www.twitter.com/MathieuLePuil" target="_blank"><img src="../assets/img/logo_x.png" class="w-4 sm:!w-8 mb-2 hover:scale-105 transition-all duration-200 ease" alt="Logo TwitterX" /></a>
-            <div class="h-14 w-1 bg-my-black"></div>
-        </div>
-        <ul class="background z-10 hidden lg:!block">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </section>
+    <div class="w-full h-full flex flex-col items-center mt-16">
+        <h1 class="text-4xl sm:!text-6xl text-my-purple font-bold">Mentions légales</h1>
+    </div>
+
+    <div class="w-[70%] mx-[15%]">
+        <p class="mt-16 text-my-black">Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site mathieulp.fr l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.</p>
+
+        <h2 class="text-my-purple text-xl font-bold mt-16">Éditeur :</h2>
+        <p class="text-my-black mt-5 font-lg">Nom : Mathieu Le Puil<br>Adresse : 53 avenue Anatole France, 10000 Troyes<br>Statut : Entrepreneur Individuel (EI)<br>Numéro d'immatriculation au RCS : 977990977</p>
+
+        <h2 class="text-my-purple text-xl font-bold mt-16">Contact :</h2>
+        <p class="text-my-black mt-5 font-lg">Adresse e-mail : contact@mathieulp.fr<br>Numéro de téléphone : 07.82.91.70.75<br><br>Numéro d'identification à la TVA : FR72977990977</p>
+
+        <h2 class="text-my-purple text-xl font-bold mt-16">Hébergeur :</h2>
+        <p class="text-my-black mt-5 font-lg mb-[10%]">Nom ou dénomination sociale de l'hébergeur : PulseHeberg<br>Adresse : 9, Boulevard de Strasbourg
+            83000 Toulon<br>Numéro de téléphone de l'hébergeur : 04.22.14.13.60</p>
+    </div>
+
 </template>
-
-<style scoped>
-    @keyframes cube {
-        from {
-            transform: scale(0) rotate(0deg) translate(-50%, -50%);
-            opacity: 1;
-        }
-        to {
-            transform: scale(20) rotate(960deg) translate(-50%, -50%);
-            opacity: 0;
-        }
-    }
-
-    .background {
-        position: fixed;
-        width: 99vw;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-    }
-    .background li {
-        position: absolute;
-        top: 80vh;
-        left: 45vw;
-        width: 10px;
-        height: 10px;
-        border: solid 1px #dddddd;
-        color: transparent;
-        transform-origin: top left;
-        transform: scale(0) rotate(0deg) translate(-50%, -50%);
-        animation: cube 12s ease-in forwards infinite;
-        z-index: 10;
-    }
-    .background li:nth-child(0) {
-        animation-delay: 0s;
-        left: 12vw;
-        top: 81vh;
-    }
-
-    .background li:nth-child(1) {
-        animation-delay: 2s;
-        left: 33vw;
-        top: 6vh;
-    }
-
-    .background li:nth-child(2) {
-        animation-delay: 4s;
-        left: 75vw;
-        top: 61vh;
-    }
-
-    .background li:nth-child(3) {
-        animation-delay: 6s;
-        left: 20vw;
-        top: 28vh;
-    }
-
-    .background li:nth-child(4) {
-        animation-delay: 8s;
-        left: 67vw;
-        top: 83vh;
-    }
-
-    .background li:nth-child(5) {
-        animation-delay: 10s;
-        left: 84vw;
-        top: 16vh;
-    }
-
-    .background li:nth-child(6) {
-        animation-delay: 12s;
-        left: 64vw;
-        top: 19vh;
-        border-color: #ffffff;
-    }
-
-    .background li:nth-child(7) {
-        animation-delay: 14s;
-        left: 6vw;
-        top: 99vh;
-        border-color: #ffffff;
-    }
-
-    .background li:nth-child(8) {
-        animation-delay: 16s;
-        left: 27vw;
-        top: 57vh;
-    }
-
-    .background li:nth-child(9) {
-        animation-delay: 18s;
-        left: 52vw;
-        top: 70vh;
-    }
-
-    .background li:nth-child(10) {
-        animation-delay: 20s;
-        left: 96vw;
-        top: 39vh;
-    }
-
-    .background li:nth-child(11) {
-        animation-delay: 22s;
-        left: 47vw;
-        top: 44vh;
-        border-color: #ffffff;
-    }
-</style>
-<script setup>
-</script>
