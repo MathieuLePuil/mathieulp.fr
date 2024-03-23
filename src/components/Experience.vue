1b277c367043c8880bc6879886c6e35cdde1956b
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-03-23 15:49:49 +0100

    feat: reformat all with Prettier

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,37 +1,61 @@
 <template>
-    <section id="experience" class="h-m-screen lg:!h-screen pt-[8vh] lg:!pt-0 pb-20 flex flex-col justify-around z-20 lg:!w-full">
-        <div class="flex items-center pb-20 lg:pb-0">
-            <h2 class="text-xl sm:text-3xl mt-10 lg:!text-6xl ml-[10%] text-white font-bold text-center">{{ $t('experience') }}</h2>
-        </div>
-        <div class="max-w-2xl mx-[10%] z-30">
-            <ol class="relative border-l border-white">
-                <li class="mb-10 ml-4">
-                    <div
-                        class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white">
-                    </div>
-                    <time class="mb-1 text-sm font-normal leading-none text-gray-400">{{ $t('freelancedate') }}</time>
-                    <h3 class="text-lg font-semibold text-white">{{ $t('freelancename') }}</h3>
-                    <p class="mb-4 text-base font-normal text-gray-300">{{ $t('freelancedesc') }}</p>
-                </li>
-                <li class="mb-10 ml-4">
-                    <div
-                        class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white">
-                    </div>
-                    <time class="mb-1 text-sm font-normal leading-none text-gray-400">{{ $t('popeyedate') }}</time>
-                    <h3 class="text-lg font-semibold text-white">{{ $t('popeyename') }}</h3>
-                    <p class="mb-4 text-base font-normal text-gray-300">{{ $t('popeyedesc') }}</p>
-                </li>
-                <li class="mb-10 ml-4">
-                    <div
-                        class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white">
-                    </div>
-                    <time class="mb-1 text-sm font-normal leading-none text-gray-400">{{ $t('studydate') }}</time>
-                    <h3 class="text-lg font-semibold text-white">{{ $t('studyname') }}</h3>
-                    <p class="mb-4 text-base font-normal text-gray-300">{{ $t('studydesc') }}</p>
-                </li>
-            </ol>
-        </div>
-    </section>
+  <section
+    id="experience"
+    class="h-m-screen lg:!h-screen pt-[8vh] lg:!pt-0 pb-20 flex flex-col justify-around z-20 lg:!w-full"
+  >
+    <div class="flex items-center pb-20 lg:pb-0">
+      <h2
+        class="text-xl sm:text-3xl mt-10 lg:!text-6xl ml-[10%] text-white font-bold text-center"
+      >
+        {{ $t("experience") }}
+      </h2>
+    </div>
+    <div class="max-w-2xl mx-[10%] z-30">
+      <ol class="relative border-l border-white">
+        <li class="mb-10 ml-4">
+          <div
+            class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white"
+          ></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400">{{
+            $t("freelancedate")
+          }}</time>
+          <h3 class="text-lg font-semibold text-white">
+            {{ $t("freelancename") }}
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-300">
+            {{ $t("freelancedesc") }}
+          </p>
+        </li>
+        <li class="mb-10 ml-4">
+          <div
+            class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white"
+          ></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400">{{
+            $t("popeyedate")
+          }}</time>
+          <h3 class="text-lg font-semibold text-white">
+            {{ $t("popeyename") }}
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-300">
+            {{ $t("popeyedesc") }}
+          </p>
+        </li>
+        <li class="mb-10 ml-4">
+          <div
+            class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white"
+          ></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-400">{{
+            $t("studydate")
+          }}</time>
+          <h3 class="text-lg font-semibold text-white">
+            {{ $t("studyname") }}
+          </h3>
+          <p class="mb-4 text-base font-normal text-gray-300">
+            {{ $t("studydesc") }}
+          </p>
+        </li>
+      </ol>
+    </div>
+  </section>
 </template>
-<script setup>
-</script>
+<script setup></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,15 @@
 <script setup>
-    import Home from '../components/Home.vue'
-    import Project from '../components/Project.vue'
-    import Experience from '../components/Experience.vue'
-    import Contact from '../components/Contact.vue'
+import Home from "../components/Home.vue";
+import Project from "../components/Project.vue";
+import Experience from "../components/Experience.vue";
+import Contact from "../components/Contact.vue";
 </script>
 
 <template>
-    <main>
-        <Home />
-        <Project />
-        <Experience />
-        <Contact />
-    </main>
+  <main>
+    <Home />
+    <Project />
+    <Experience />
+    <Contact />
+  </main>
 </template>
